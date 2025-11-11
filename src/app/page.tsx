b5c669ef9b0a4aa9f54559881317657c56ce87cb
Victor Sanches <victor.sm1208@gmail.com>
2025-11-11 16:45:26 -0300

    aula 106

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
-import clsx from 'clsx';
+import { Header } from '@/components/Header';
 
-export default function HomePage() {
+// page.tsx (server) -> menu.tsx (server) -> link (client)
+
+export default async function HomePage() {
+  console.log('HomePage');
+
   return (
     <div>
-      <h1
-        className={clsx(
-          'text-6xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-blue-50',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-1000',
-        )}
-      >
-        Texto no meu h1
-      </h1>
+      <Header />
     </div>
   );
 }
