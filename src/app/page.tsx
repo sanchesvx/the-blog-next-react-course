e0363db96820420499769a3b6aee73e1bc74dee4
Victor Sanches <victor.sm1208@gmail.com>
2025-11-13 01:52:48 -0300

    aula 113

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import clsx from 'clsx';
+import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <div>
-      <SpinLoader containerClasses={clsx('min-h-[500px]', 'bg-amber-500')} />
+      <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className='text-6xl font-bold text-center py-8'>Footer</p>
+      </footer>
     </div>
   );
 }
