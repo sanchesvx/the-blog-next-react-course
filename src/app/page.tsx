31a5208448b564e02a3b9ee61586abb0dd641512
Victor Sanches <victor.sm1208@gmail.com>
2025-11-14 03:41:08 -0300

    aula 118

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,32 @@ export default async function HomePage() {
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
         <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
           <Image
-            className='group-hover:scale-105 transition'
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Título do post'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-          dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
-          ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
-          mollitia?
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-04-20'
+          >
+            20/04/2025 10:00
+          </time>
+
+          <h1 className='text-2xl/tight font-extrabold sm:text-4xl'>
+            <Link href='#'>Lorem ipsum dolor sit amet consectetur</Link>
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+            dolorem est dolor porro, doloribus neque, quidem mollitia
+            doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
+            harum blanditiis mollitia?
+          </p>
         </div>
       </section>
 
