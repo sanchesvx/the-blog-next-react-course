14a77186a6041edb20d0252a99b6c6f3a12facbb
Victor Sanches <victor.sm1208@gmail.com>
2025-11-26 20:19:26 -0300

    aula 141

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import { ClientComponent } from '@/components/ClientComponent';
 import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
+import { ServerComponent } from '@/components/ServerComponent';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <>
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+
       <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
         <PostFeatured />
         <PostsList />
