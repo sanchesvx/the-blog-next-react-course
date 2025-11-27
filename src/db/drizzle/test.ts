9e9a5f6f5070baa2e8b3e118883f5fcb3caff5db
Victor Sanches <victor.sm1208@gmail.com>
2025-11-27 16:45:19 -0300

    aula 149

--- src/db/drizzle/test.ts
+++ src/db/drizzle/test.ts
@@ -1,8 +1,10 @@
-import { drizzleDb } from '.';
-import { postsTable } from './schemas';
+// import { drizzleDb } from '.';
+// import { postsTable } from './schemas';
 
-(async () => {
-  const posts = await drizzleDb.select().from(postsTable);
+// (async () => {
+//   const posts = await drizzleDb.select().from(postsTable);
 
-  console.log(posts);
-})();
+//   posts.forEach(post => {
+//     console.log(post.slug);
+//   });
+// })();
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -36,6 +36,15 @@ export class JsonPostRepository implements PostRepository {
     return posts.filter(post => post.published);
   }
 
+  async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
+    console.log('\n', 'findAll', '\n');
+
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
   async findById(id: string): Promise<PostModel> {
     const posts = await this.findAllPublic();
     const post = posts.find(post => post.id === id);
