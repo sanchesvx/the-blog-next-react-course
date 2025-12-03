5c0f330877b967e4964c1b649b89a0f26c62becb
Victor Sanches <victor.sm1208@gmail.com>
2025-12-03 15:41:49 -0300

    aula 164

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -3,19 +3,21 @@ import { unstable_cache } from 'next/cache';
 import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
-export const findAllPublicPostsCached = unstable_cache(
-  cache(async () => {
-    return await postRepository.findAllPublic();
-  }),
-  ['posts'],
-  {
-    tags: ['posts'],
-  },
+export const findAllPublicPostsCached = cache(
+  unstable_cache(
+    async () => {
+      return await postRepository.findAllPublic();
+    },
+    ['posts'],
+    {
+      tags: ['posts'],
+    },
+  ),
 );
 
-export const findPublicPostBySlugCached = (slug: string) =>
-  unstable_cache(
-    cache(async (slug: string) => {
+export const findPublicPostBySlugCached = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
       const post = await postRepository
         .findBySlugPublic(slug)
         .catch(() => undefined);
@@ -23,9 +25,8 @@ export const findPublicPostBySlugCached = (slug: string) =>
       if (!post) notFound();
 
       return post;
-    }),
-    ['posts'],
-    {
-      tags: [`post-${slug}`],
     },
+    [`post-${slug}`],
+    { tags: [`post-${slug}`] },
   )(slug);
+});
--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,10 +1,11 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
+import { logColor } from '@/utils/log-color';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
-    console.log('\n', 'D findAllPublic', '\n');
+    logColor('findAllPublic', Date.now());
 
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
@@ -15,7 +16,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
-    console.log('\n', 'D findBySlugPublic', '\n');
+    logColor('findBySlugPublic', Date.now());
 
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq, and }) =>
@@ -28,7 +29,8 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
-    console.log('\n', 'D findAll', '\n');
+    logColor('findAll', Date.now());
+
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
     });
@@ -37,7 +39,8 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findById(id: string): Promise<PostModel> {
-    console.log('\n', 'D findById', '\n');
+    logColor('findById', Date.now());
+
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq }) => eq(posts.id, id),
     });
@@ -47,3 +50,17 @@ export class DrizzlePostRepository implements PostRepository {
     return post;
   }
 }
+
+// (async () => {
+//   //   como-a-tecnologia-impacta-nosso-bem-estar false
+//   // os-desafios-do-trabalho-remoto-moderno true
+//   //   6b204dab-2312-4525-820a-a0463560835f false
+//   // 76396dd3-9581-43b5-856d-fe1a78714e8c true
+//   const repo = new DrizzlePostRepository();
+//   // const posts = await repo.findAllPublic();
+//   // posts.forEach(post => console.log(post.id, post.published));
+//   const post = await repo.findBySlugPublic(
+//     'os-desafios-do-trabalho-remoto-moderno ',
+//   );
+//   console.log(post);
+// })();
