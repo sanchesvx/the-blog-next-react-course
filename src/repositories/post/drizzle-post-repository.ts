77a7b749e72a11db6737cddfd4ff890c4996aad0
Victor Sanches <victor.sm1208@gmail.com>
2025-12-03 16:05:37 -0300

    aula 165

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -2,9 +2,12 @@ import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
 import { logColor } from '@/utils/log-color';
+import { asyncDelay } from '@/utils/async-delay';
+import { SIMULATE_WAIT_IN_MS } from '@/lib/constants';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
     logColor('findAllPublic', Date.now());
 
     const posts = await drizzleDb.query.posts.findMany({
@@ -16,6 +19,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
     logColor('findBySlugPublic', Date.now());
 
     const post = await drizzleDb.query.posts.findFirst({
@@ -29,6 +33,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
     logColor('findAll', Date.now());
 
     const posts = await drizzleDb.query.posts.findMany({
@@ -39,6 +44,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findById(id: string): Promise<PostModel> {
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
     logColor('findById', Date.now());
 
     const post = await drizzleDb.query.posts.findFirst({
@@ -50,17 +56,3 @@ export class DrizzlePostRepository implements PostRepository {
     return post;
   }
 }
-
-// (async () => {
-//   //   como-a-tecnologia-impacta-nosso-bem-estar false
-//   // os-desafios-do-trabalho-remoto-moderno true
-//   //   6b204dab-2312-4525-820a-a0463560835f false
-//   // 76396dd3-9581-43b5-856d-fe1a78714e8c true
-//   const repo = new DrizzlePostRepository();
-//   // const posts = await repo.findAllPublic();
-//   // posts.forEach(post => console.log(post.id, post.published));
-//   const post = await repo.findBySlugPublic(
-//     'os-desafios-do-trabalho-remoto-moderno ',
-//   );
-//   console.log(post);
-// })();
