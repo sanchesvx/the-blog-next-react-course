4bb074545002c26a9e258fcb2862b6c9c27bfd33
Victor Sanches <victor.sm1208@gmail.com>
2025-12-02 23:36:50 -0300

    aula 155

---
+++ src/app/exemplo/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { formatHour } from '@/utils/format-datetime';
+
+// export const dynamic = 'force-static';
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }];
+}
+
+export default async function ExemploDynamicPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const hour = formatHour(Date.now());
+
+  return (
+    <main className='min-h-[600px] text-4xl font-bold'>
+      <div>
+        Hora: {hour} (ID: {id})
+      </div>
+    </main>
+  );
+}
