b318b48d6b502dd3985a80a8ebf57ccd9509d961
Victor Sanches <victor.sm1208@gmail.com>
2025-12-04 14:01:26 -0300

    aula 177

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,8 +4,16 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16'>
-      <Button type='submit'>Funciona como o do JSX</Button>
+    <div className='py-16 flex gap-4 flex-wrap'>
+      <Button variant='default' size='sm'>
+        Confirma
+      </Button>
+      <Button variant='ghost' size='md'>
+        Confirma
+      </Button>
+      <Button variant='danger' size='lg'>
+        Confirma
+      </Button>
     </div>
   );
 }
