42df7a310bdb01cf390898474150153b3814dfd2
Victor Sanches <victor.sm1208@gmail.com>
2025-12-04 13:55:06 -0300

    aula 176

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/Button';
+
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return <div className='py-16 text-6xl'>AdminPostNewPage</div>;
+  return (
+    <div className='py-16'>
+      <Button type='submit'>Funciona como o do JSX</Button>
+    </div>
+  );
 }
