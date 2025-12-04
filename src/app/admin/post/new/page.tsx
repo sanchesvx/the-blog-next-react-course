db9dd73188115cd6a039187b0182b3ab8f2fe07b
Victor Sanches <victor.sm1208@gmail.com>
2025-12-04 14:39:08 -0300

    aula 180

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,38 +1,49 @@
+import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='flex flex-col gap-6'>
-      <InputText
-        labelText='Nome'
-        placeholder='Digite seu nome'
-        type='password'
-      />
-      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        defaultValue='Olá mundo'
-      />
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        readOnly
-      />
-      <InputText
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        defaultValue='Olá mundo'
-        readOnly
-      />
-    </div>
+    <form action='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputText
+          labelText='Nome'
+          placeholder='Digite seu nome'
+          type='password'
+        />
+        <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+
+        <InputCheckbox labelText='Sobrenome' />
+
+        <InputText
+          disabled
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+          defaultValue='Olá mundo'
+        />
+        <InputText
+          disabled
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+        />
+        <InputText
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+          readOnly
+        />
+        <InputText
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+          defaultValue='Olá mundo'
+          readOnly
+        />
+
+        <div className='mt-4'>
+          <Button type='submit'>Enviar</Button>
+        </div>
+      </div>
+    </form>
   );
 }
