ee9de85314f4a19676579f54eac09af42960d285
Victor Sanches <victor.sm1208@gmail.com>
2025-12-03 23:00:12 -0300

    aula 174

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,13 +1,69 @@
+import clsx from 'clsx';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    'bg-slate-900 text-slate-100 rounded-lg',
+    'flex flex-col  mb-8',
+    'sm:flex-row sm:flex-wrap',
+    // 'h-10',
+    // 'overflow-hidden',
+  );
+  const linkClasses = clsx(
+    '[&>svg]:w-4 [&>svg]:h-4 px-4',
+    'flex items-center justify-start gap-2',
+    'transition hover:bg-slate-800 rounded-lg',
+    'h-10',
+    'shrink-0',
+  );
+
   return (
-    <nav>
-      <a href='/' target='_blank'>
+    <nav className={navClasses}>
+      <a className={linkClasses} href='/' target='_blank'>
+        <HouseIcon />
         Home
       </a>
 
-      <Link href='/admin/post'>Posts</Link>
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
