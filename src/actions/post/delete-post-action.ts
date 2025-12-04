fe4b3d0d30eaec740cbe7f2a29103e11c7ab112d
Victor Sanches <victor.sm1208@gmail.com>
2025-12-03 21:22:13 -0300

    aula 171

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,11 +1,42 @@
 'use server';
 
+import { drizzleDb } from '@/db/drizzle';
+import { postsTable } from '@/db/drizzle/schemas';
+import { postRepository } from '@/repositories/post';
 import { asyncDelay } from '@/utils/async-delay';
 import { logColor } from '@/utils/log-color';
+import { eq } from 'drizzle-orm';
+import { revalidateTag } from 'next/cache';
 
 export async function deletePostAction(id: string) {
-  await asyncDelay(2000);
+  // TODO: checar login do usuário
 
+  // TODO: remover linhas abaixo
+  await asyncDelay(2000);
   logColor('' + id);
-  return id;
+
+  if (!id || typeof id !== 'string') {
+    return {
+      error: 'Dados inválidos',
+    };
+  }
+
+  const post = await postRepository.findById(id).catch(() => undefined);
+
+  if (!post) {
+    return {
+      error: 'Post não existe',
+    };
+  }
+
+  // TODO: mover este método para o repositório
+  await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
+
+  // TODO: revalidateTag ou revalidatePath
+  revalidateTag('posts', 'default');
+  revalidateTag(`post-${post.slug}`, 'default');
+
+  return {
+    error: '',
+  };
 }
--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -2,9 +2,15 @@ import { findAllPostAdmin } from '@/lib/post/queries/admin';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { DeletePostButton } from '../admin/DeletePostButton';
+import ErrorMessage from '../ErrorMessage';
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
+
+  if (posts.length <= 0)
+    return (
+      <ErrorMessage contentTitle='Ei 😅' content='Bora criar algum post??' />
+    );
 
   return (
     <div className='mb-16'>
