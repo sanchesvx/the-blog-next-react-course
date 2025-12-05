684dcc5ac807ea11a18a90c34833d82024ddde5f
Victor Sanches <victor.sm1208@gmail.com>
2025-12-04 21:24:27 -0300

    aula 188

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -4,12 +4,13 @@ import { uploadImageAction } from '@/actions/upload/upload-image-reaction';
 import { Button } from '@/components/Button';
 import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
-import { useRef, useTransition } from 'react';
+import { useRef, useState, useTransition } from 'react';
 import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [isUploading, startTransition] = useTransition();
+  const [imgUrl, setImgUrl] = useState('');
 
   function handleChooseFile() {
     if (!fileInputRef.current) return;
@@ -19,12 +20,18 @@ export function ImageUploader() {
 
   function handleChange() {
     toast.dismiss();
-    if (!fileInputRef.current) return;
+    if (!fileInputRef.current) {
+      setImgUrl('');
+      return;
+    }
 
     const fileInput = fileInputRef.current;
     const file = fileInput?.files?.[0];
 
-    if (!file) return;
+    if (!file) {
+      setImgUrl('');
+      return;
+    }
 
     if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
       const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
@@ -43,22 +50,39 @@ export function ImageUploader() {
       if (result.error) {
         toast.error(result.error);
         fileInput.value = '';
+        setImgUrl('');
         return;
       }
 
-      // TODO: continuar depois
-      toast.success(result.url);
+      setImgUrl(result.url);
+      toast.success('Imagem enviada');
     });
 
     fileInput.value = '';
   }
 
   return (
-    <div className='flex flex-col gap-2 py-4'>
-      <Button onClick={handleChooseFile} type='button' className='self-start'>
+    <div className='flex flex-col gap-4 py-4'>
+      <Button
+        onClick={handleChooseFile}
+        type='button'
+        className='self-start'
+        disabled={isUploading}
+      >
         <ImageUpIcon />
         Enviar uma imagem
       </Button>
+
+      {!!imgUrl && (
+        <div className='flex flex-col gap-4'>
+          <p>
+            <b>URL:</b> {imgUrl}
+          </p>
+
+          {/* eslint-disable-next-line */}
+          <img className='rounded-lg' src={imgUrl} />
+        </div>
+      )}
 
       <input
         onChange={handleChange}
