0ba1dbf4fb9977b4dda8ded6cbf355d93bf51f06
Victor Sanches <victor.sm1208@gmail.com>
2025-12-05 13:38:32 -0300

    aula 195

---
+++ src/actions/post/create-post-action.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { makePartialPublicPost, PublicPost } from '@/dto/post/dto';
+import { PostCreateSchema } from '@/lib/post/validations';
+import { PostModel } from '@/models/post/post-model';
+import { getZodErrorMessages } from '@/utils/is-url-or-relative-path';
+
+type CreatePostActionState = {
+  formState: PublicPost;
+  errors: string[];
+};
+
+export async function createPostAction(
+  prevState: CreatePostActionState,
+  formData: FormData,
+): Promise<CreatePostActionState> {
+  // TODO: verificar se o usuário tá logado
+
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ['Dados inválidos'],
+    };
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries());
+  const zodParsedObj = PostCreateSchema.safeParse(formDataToObj);
+
+  if (!zodParsedObj.success) {
+    const errors = getZodErrorMessages(zodParsedObj.error);
+    return {
+      errors,
+      formState: makePartialPublicPost(formDataToObj),
+    };
+  }
+
+  const validPostData = zodParsedObj.data;
+  const newPost: PostModel = {
+    ...validPostData,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: Date.now().toString(),
+    slug: Math.random().toString(36),
+  };
+
+  return {
+    formState: newPost,
+    errors: [],
+  };
+}
