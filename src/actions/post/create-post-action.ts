b0edb1134521bd3b152d0944a12f4b2add3bddef
Victor Sanches <victor.sm1208@gmail.com>
2025-12-05 13:43:21 -0300

    aula 196

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -4,6 +4,8 @@ import { makePartialPublicPost, PublicPost } from '@/dto/post/dto';
 import { PostCreateSchema } from '@/lib/post/validations';
 import { PostModel } from '@/models/post/post-model';
 import { getZodErrorMessages } from '@/utils/is-url-or-relative-path';
+import { makeSlugFromText } from '@/utils/make-slug-from-text';
+import { v4 as uuidV4 } from 'uuid';
 
 type CreatePostActionState = {
   formState: PublicPost;
@@ -39,8 +41,8 @@ export async function createPostAction(
     ...validPostData,
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
-    id: Date.now().toString(),
-    slug: Math.random().toString(36),
+    id: uuidV4(),
+    slug: makeSlugFromText(validPostData.title),
   };
 
   return {
