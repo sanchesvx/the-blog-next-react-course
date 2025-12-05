7cc047da83532f3adbc34f9519815124b1155a47
Victor Sanches <victor.sm1208@gmail.com>
2025-12-05 14:17:37 -0300

    aula 197

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,10 +1,14 @@
 'use server';
 
+import { drizzleDb } from '@/db/drizzle';
+import { postsTable } from '@/db/drizzle/schemas';
 import { makePartialPublicPost, PublicPost } from '@/dto/post/dto';
 import { PostCreateSchema } from '@/lib/post/validations';
 import { PostModel } from '@/models/post/post-model';
 import { getZodErrorMessages } from '@/utils/is-url-or-relative-path';
 import { makeSlugFromText } from '@/utils/make-slug-from-text';
+import { revalidateTag } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { v4 as uuidV4 } from 'uuid';
 
 type CreatePostActionState = {
@@ -45,8 +49,9 @@ export async function createPostAction(
     slug: makeSlugFromText(validPostData.title),
   };
 
-  return {
-    formState: newPost,
-    errors: [],
-  };
+  // TODO: mover este método para o repositório
+  await drizzleDb.insert(postsTable).values(newPost);
+
+  revalidateTag('posts', 'default');
+  redirect(`/admin/post/${newPost.id}`);
 }
