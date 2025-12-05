c5262b49760163c8986725c1a37b8d70602651a5
Victor Sanches <victor.sm1208@gmail.com>
2025-12-04 21:19:46 -0300

    aula 187

--- src/actions/upload/upload-image-reaction.ts
+++ src/actions/upload/upload-image-reaction.ts
@@ -1,6 +1,12 @@
 'use server';
 
-import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIRECTORY,
+  IMAGE_UPLOAD_MAX_SIZE,
+} from '@/lib/constants';
+import { mkdir, writeFile } from 'fs/promises';
+import { extname, resolve } from 'path';
 
 type UploadImageActionResult = {
   url: string;
@@ -30,6 +36,24 @@ export async function uploadImageAction(
     return makeResult({ error: 'Imagem inválida' });
   }
 
-  // TODO: enviei o arquivo
-  return makeResult({ url: 'URL' });
+  const imageExtension = extname(file.name);
+  const uniqueImageName = `${Date.now()}${imageExtension}`;
+
+  const uploadFullPath = resolve(
+    process.cwd(),
+    'public',
+    IMAGE_UPLOAD_DIRECTORY,
+  );
+  await mkdir(uploadFullPath, { recursive: true });
+
+  const fileArrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrayBuffer);
+
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
+
+  return makeResult({ url });
 }
