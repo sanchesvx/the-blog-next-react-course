fa65cb3ecfcb77df27c6a701e59ab5757ed85df5
Victor Sanches <victor.sm1208@gmail.com>
2025-12-06 22:15:58 -0300

    aula 205

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -9,8 +9,24 @@ type LoginActionState = {
 
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(5000);
+
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  // Dados que o usuário digitou no form
+  const username = formData.get('username')?.toString() || '';
+  const password = formData.get('password')?.toString() || '';
+
+  // Aqui eu checaria se o usuário existe na base de dados
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = '';
+
   return {
     username: '',
-    error: 'Teste de erro',
+    error: '',
   };
 }
